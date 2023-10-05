f92a12acbf28afad10e6e80dcc3f6b4e39c3633f
Vanitha751 <xworkzvanitha@gmail.com>
2023-10-05 19:48:17 +0530

    first program

---
+++ Array.java
@@ -0,0 +1,11 @@
+class Array{
+  static int[]number={5,2,3};
+
+public static void main(String args[]){
+
+System.out.println(Array.number[1]);
+System.out.println(Array.number[2]);
+System.out.println(Array.number[0]);
+}
+
+}
